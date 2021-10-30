770d3361386af829a46b5561bf317e575376f9fc
Troy Scott <troys2005@gmail.com>
2021-10-30 15:02:15 -0700

    initial check in

---
+++ ch1/interest.py
@@ -0,0 +1,12 @@
+# interest.py
+
+principal = 1000
+rate = 0.05
+numyears = 5
+year = 1
+
+while year < numyears:
+    principal = principal * ( 1  + rate)
+    print(year, principal)
+    year += 1
+
